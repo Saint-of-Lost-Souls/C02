57fb0b8d785db55d523ea2a9c3265a4ea1bd9b7e
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-03 21:41:20 +1030

    Upload

---
+++ ex02/ft_str_is_alpha.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int     ft_str_is_alpha(char*str)
+{
+
+    Regex.IsMatch(input, @"^[a-zA-Z]+$");
+}
+
+int main()
+{
+
+    char string_1[] = "The cake is a lie";
+    char string_2[] = "The cake is a f*$#ing lie";
+    char string_3[] = "The cake is 1 serving";
+
+    ft_str_is_alpha(string);
+    return 0;
+}

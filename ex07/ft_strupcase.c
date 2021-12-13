f221c9e9d90bfe91f9eaf743e0be40ad39eca4d6
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-13 13:33:05 +1030

    update

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -4,7 +4,6 @@ char    ft_strupcase(char *str)
 {
         int i;
 
-        i = 0;
         while (str[i] != '\0')
         {
                 if (str[i] >= 'a' && str[i] <= 'z')
--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -4,7 +4,6 @@ char    ft_strlowcase(char *str)
 {        
         int i;
 
-        i = 0;
         while (str[i] != '\0')
         {
                 if (str[i] >= 'A' && str[i] <= 'Z')
